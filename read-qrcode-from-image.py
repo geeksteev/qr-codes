6608418d0fd9ec34b29daa1faaa37418dc793642
steev <binaryhansolo@gmail.com>
2023-09-27 12:58:30 -0400

    File cleanup

--- read-qrcode-from-image.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img = cv2.imread('shirt3.png')
-detect = cv2.QRCodeDetector()
-retval, decoded_info, points, straight_qrcode = detect.detectAndDecodeMulti(img)
-
-print(detect.detectAndDecodeMulti(img))
