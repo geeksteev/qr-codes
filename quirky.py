da6c70a78ba303fe5b8c8e0dfb89fff1e8a57aa3
steev <binaryhansolo@gmail.com>
2023-09-27 13:03:29 -0400

    Changed the name of the argument "create" to "generate"

--- quirky.py
+++ quirky.py
@@ -9,7 +9,7 @@ def readQRCodeFromImage(filename):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-g', '--generate', help='Create a QR Code', required=False)
+    parser.add_argument('-g', '--generate', help='Generate a QR Code', required=False)
     parser.add_argument('-r', '--read', help='Read a QR Code', required=False)
     parser.add_argument('-f', '--file', help='Read a QR Code', required=False)
     parser.add_argument('-u', '--url', help='Enter the URL for the QR code', required=False)
