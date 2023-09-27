cbf83f27fdad449fd206712227e64c03718d85d8
Steev <binaryhansolo@gmail.com>
2023-09-27 12:06:19 -0400

    Removed some extra files

--- generate-guest-wifi-code.py
+++
@@ -1,6 +0,0 @@
-import segno
-
-wifi_code = helpers.make_wifi(ssid='Partridge Guest', password='WiFiPassword22', security='WPA')
-wifi_code.designator
-'3-M'
-wifi_code.save('partridge-guest-wifi.png', scale=10)
